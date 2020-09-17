a3ff445e06d365c09cb43d7c18b26812c10996da
Connor Kelleher <connor.r.kelleher@gmail.com>
2020-09-17 11:10:39 -0400

    Handled output_len option to make longer collages

--- py_sound_collage/cli.py
+++ py_sound_collage/cli.py
@@ -18,33 +18,43 @@ import os
 @click.option(
     "--output_len",
     type=int,
-    help="Length in milliseconds of output collage")
-@click.option(
-    '--repeat', 
-    is_flag=True,
-    default=False,
-    help="Repeat input songs")
-def cli(audio_dir, sample_len, repeat, output_len):
+    help="Length in milliseconds of output collage. NOTE: Songs can repeat with this option")
+def cli(audio_dir, sample_len, output_len):
     """Generate audio collage with random snippets local files
+
+    Concatenates random segments of length sample_len ms from mp3 files 
+    located in audio_dir. 
+    NOTE: If OUTPUT_LEN is not specified each input file will only 
+    be sampled once. When OUTPUT_LEN is specified each input file 
+    may be sampled from more than once or not at all
 
     AUDIO_DIR is the directory to search for audio files
     """
     os.chdir(audio_dir)
-    print(f"reading in songs")
     all_songs = [AudioSegment.from_mp3(mp3_file) for mp3_file in glob("*.mp3")]
-    #print(f"flattening audio segments")
-    #big_fat_sample = [item for sublist in all_songs for item in sublist]
-    print(f"Original list of songs: {all_songs}")
     random.shuffle(all_songs)
-    print(f"list after shuffle: {all_songs}")
+    click.echo(f"Found {len(all_songs)} songs")
+    print(f"output_len = {output_len}")
 
     collage = AudioSegment.silent(1) 
-    for song in all_songs:
+    # generate output_len ms of audio
+    if output_len:
+      while len(collage) < output_len:
+        song = random.choice(all_songs)
         start = random.randint(0, len(song) - sample_len)
-        clip = song[start: start + sample_len]
+        end = min(start + sample_len, len(song))
+        clip = song[start : end]
         collage = collage.append(clip, crossfade=0)
+    # sample each song once
+    else:
+      for song in all_songs:
+          start = random.randint(0, len(song) - sample_len)
+          end = min(start + sample_len, len(song))
+          clip = song[start : end]
+          collage = collage.append(clip, crossfade=0)
 
+    # write output
     collage_length = len(collage) / 1000 # ms
-    print(f"Length of all mp3s: {collage_length} sec")
+    click.echo(f"Collage length: {collage_length} sec")
     collage.export("collage.mp3", format="mp3")
 
